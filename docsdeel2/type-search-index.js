a2227354c823a787bbe839e692680a0f2bec7d49
Marc De Caluwé <57325815+marcdc-tm@users.noreply.github.com>
2022-01-03 19:30:29 +0100

    Delete docsdeel2 directory

--- docsdeel2/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"oop.test2.deel2","l":"Entity"},{"p":"oop.test2.deel2","l":"IHasIdentifier"},{"p":"oop.test2.deel2","l":"IHasName"},{"p":"oop.test2.deel2","l":"IHasTitle"},{"p":"oop.test2.deel2","l":"Stub"}]
