d022f7e9e2c74ff34c3e0bd7856dfb149f3f32c9
Marc De Caluwé <57325815+marcdc-tm@users.noreply.github.com>
2022-01-03 19:31:50 +0100

    Add files via upload
    oop: docs deel2 van test2

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"oop.test2","l":"Entity"},{"p":"oop.test2","l":"IHasIdentifier"},{"p":"oop.test2","l":"IHasName"},{"p":"oop.test2","l":"IHasTitle"},{"p":"oop.test2","l":"Stub"}]
